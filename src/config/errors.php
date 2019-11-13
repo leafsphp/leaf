38f0480d8c032673e6461fd2f7090cb01faf0d3d
mr-phlames <mickdd22@gmail.com>
2019-11-13 09:36:59 +0000

    added editor ... to Leaf Dev errors

--- src/config/errors.php
+++ src/config/errors.php
@@ -1,12 +1,23 @@
 <?php
 namespace Leaf\Config;
 
+/**
+ * Leaf Dev Errors
+ * ------
+ * Easily handle run-time errors
+ */
 class Errors {
+	/**
+	 * Hide run time errors
+	 */
 	public function hide() {
 		error_reporting(0);
    		ini_set('display_errors', 0);
 	}
 
+	/**
+	 * Show run time errors
+	 */
 	public function show() {
 		error_reporting(1);
    		ini_set('display_errors', 1);
