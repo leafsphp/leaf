3bb0e5ed46103528fbfc34d07e9cdde962d19e67
Michael Darko <mickdd22@gmail.com>
2021-11-09 09:22:15 +0000

    :bug: fixed import URL

--- src/Exception/General.php
+++ src/Exception/General.php
@@ -151,33 +151,33 @@ class General extends \Exception
         return static::exceptionMarkup($title, $body);
     }
 
-    /**
-     * Generate diagnostic template markup
-     *
-     * This method accepts a title and body content to generate an HTML document layout.
-     *
-     * @param  string $title The title of the HTML template
-     * @param  string $body The body content of the HTML template
-     * @return string
-     */
-    protected static function errorMarkup($title, $body)
-    {
-		return "<html><head><title>$title</title><link rel=\"stylesheet\" href=\"'https://fonts.googleapis.com/css?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700;display=swap'\"><style>body{display:flex;justify-content:center;align-items:center;background-color:rgb(0,30,38);color:white;margin:0;padding:0px 30px;font-family:DM Sans,sans-serif;}h1{margin:0;font-weight:normal;}strong{display:inline-block;width:65px;}</style></head><body><h1 style=\"color: #34be6d;border-right:1px solid #555855;padding-right:20px;\">$title</h1><main style=\"padding-left:20px;\">$body</main></body></html>";
-    }
+	/**
+	 * Generate diagnostic template markup
+	 *
+	 * This method accepts a title and body content to generate an HTML document layout.
+	 *
+	 * @param  string $title The title of the HTML template
+	 * @param  string $body The body content of the HTML template
+	 * @return string
+	 */
+	protected static function errorMarkup($title, $body)
+	{
+		return "<html><head><title>$title</title><link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700;display=swap\"><style>body{display:flex;justify-content:center;align-items:center;background-color:rgb(0,30,38);color:white;margin:0;padding:0px 30px;font-family:DM Sans,sans-serif;}h1{margin:0;font-weight:normal;}strong{display:inline-block;width:65px;}</style></head><body><h1 style=\"color: #34be6d;border-right:1px solid #555855;padding-right:20px;\">$title</h1><main style=\"padding-left:20px;\">$body</main></body></html>";
+	}
 
-    /**
-     * Generate diagnostic template markup
-     *
-     * This method accepts a title and body content to generate an HTML document layout.
-     *
-     * @param  string $title The title of the HTML template
-     * @param  string $body The body content of the HTML template
-     * @return string
-     */
-    protected static function exceptionMarkup($title, $body)
-    {
-		return "<html><head><title>$title</title><link rel=\"stylesheet\" href=\"'https://fonts.googleapis.com/css?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700;display=swap'\"><style>body{background-color:rgb(0,30,38);color:white;margin:0;padding:50px;font:15px/14px DM Sans,sans-serif;}h1{margin:0;font-size:48px;font-weight:normal;line-height:48px;}h2{margin-top:70px;}strong{color:#34be6d;display:inline-block;width:65px;}div{margin:15px 0px;}div strong{margin-right:40px;}</style></head><body>$body</body></html>";
-    }
+	/**
+	 * Generate diagnostic template markup
+	 *
+	 * This method accepts a title and body content to generate an HTML document layout.
+	 *
+	 * @param  string $title The title of the HTML template
+	 * @param  string $body The body content of the HTML template
+	 * @return string
+	 */
+	protected static function exceptionMarkup($title, $body)
+	{
+		return "<html><head><title>$title</title><link rel=\"stylesheet\" href=\"https://fonts.googleapis.com/css?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700;display=swap\"><style>body{background-color:rgb(0,30,38);color:white;margin:0;padding:50px;font:15px/14px DM Sans,sans-serif;}h1{margin:0;font-size:48px;font-weight:normal;line-height:48px;}h2{margin-top:70px;}strong{color:#34be6d;display:inline-block;width:65px;}div{margin:15px 0px;}div strong{margin-right:40px;}</style></head><body>$body</body></html>";
+	}
 
     /**
      * Default Not Found handler
