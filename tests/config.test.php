037c1260182ae99c3c8d1367658a5da03286443c
Michael Darko <mickdd22@gmail.com>
2021-12-31 12:36:27 +0000

    :wrench: added nested config

--- tests/config.test.php
+++ tests/config.test.php
@@ -19,3 +19,13 @@ test('centralized config after init', function () {
 
 	expect($appMode)->toBe($testMode);
 });
+
+test('nested config', function () {
+	app()->config('app.key', '2');
+
+	$appConfig = app()->config('app');
+
+	expect(isset($appConfig['key']))->toBeTrue();
+	expect($appConfig['key'])->toBe('2');
+	expect(app()->config('app.key'))->toBe('2');
+});
