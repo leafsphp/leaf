1209c52a1fc8ee7c604ada4d93b4ab1f4d8f0a2d
mychidarko <mickdd22@gmail.com>
2024-08-19 18:07:23 +0000

    test: remove nested config tests

--- tests/config.test.php
+++ tests/config.test.php
@@ -20,36 +20,6 @@ test('centralized config after init', function () {
     expect($appMode)->toBe($testMode);
 });
 
-test('nested config', function () {
-    app()->config('randomKey.number', '2');
-
-    $randomKey = app()->config('randomKey');
-
-    expect(isset($randomKey['number']))->toBeTrue();
-    expect($randomKey['number'])->toBe('2');
-    expect(app()->config('randomKey.number'))->toBe('2');
-});
-
-test('nested config (array)', function () {
-    app()->config(['nestedKey.number' => '2']);
-
-    $nestedKey = app()->config('nestedKey');
-
-    expect(isset($nestedKey['number']))->toBeTrue();
-    expect($nestedKey['number'])->toBe('2');
-    expect(app()->config('nestedKey.number'))->toBe('2');
-});
-
-test('nested config (custom group)', function () {
-    app()->config(['home.key' => '2']);
-
-    $homeConfig = app()->config('home');
-
-    expect(isset($homeConfig['key']))->toBeTrue();
-    expect($homeConfig['key'])->toBe('2');
-    expect(app()->config('home.key'))->toBe('2');
-});
-
 test('Env is successfully retrieved', function () {
     expect(_env('USER', false))->toBe(getenv('USER'));
 });
